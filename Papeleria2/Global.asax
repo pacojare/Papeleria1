2ef25d08b69055801ed327ba2b9808e2d2809acb
Francisco Jasso <pacojare_0223@hotmail.com>
2021-06-01 00:00:20 -0500

    Nuevo

---
+++ Papeleria2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Papeleria2.MvcApplication" Language="C#" %>
---
+++ Papeleria2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Papeleria2.Startup))]
+namespace Papeleria2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
