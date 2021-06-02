e06ff5d83bd7170a48e8c10537cb5c3924f29c5e
JMiguel0 <jarchundiah@outlook.com>
2021-06-02 17:59:00 -0500

    Primera parte del Carrito

---
+++ Papeleria2/Controllers/CarroController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Papeleria2.Models;
+
+namespace Papeleria2.Controllers
+{
+    public class CarroController : Controller
+    {
+        // GET: Carro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
