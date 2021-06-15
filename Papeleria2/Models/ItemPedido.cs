5076c015cca490705167de8dc1aea1435e15150f
GusR97 <gruizr@toluca.tecnm.mx>
2021-06-14 19:54:21 -0500

    Pedidos parte 1

---
+++ Papeleria2/Models/ItemPedido.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Papeleria2.Models
+{
+    public class ItemPedido
+    {
+        public int idOrd
+        {
+            get;
+            set;
+        }
+
+        public Productos Product
+        {
+            get;
+            set;
+        }
+
+        public int cantidad
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Papeleria2/Models/PedidoCliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Papeleria2.Models
+{
+    public class PedidoCliente
+    {
+
+        private PapeleriaContext db = new PapeleriaContext();
+        private List<Orden_productos> detalle_orden;
+
+        public PedidoCliente()
+        {
+            detalle_orden = db.Orden_productos.ToList();
+        }
+
+        public Ordenes Orden
+        {
+            get;
+            set;
+        }
+
+        public string Fecha
+        {
+            get;
+            set;
+        }
+
+        public string envio
+        {
+            get;
+            set;
+        }
+
+        public string status
+        {
+            get;
+            set;
+        }
+
+        public string Total
+        {
+            get;
+            set;
+        }
+
+        public List<Orden_productos> ordenProductos
+        {
+            get;
+            set;
+        }
+    }
+}
